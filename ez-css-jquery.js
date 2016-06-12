03a94536dabb2b26f6c3be0208a5e64a5534ae3d
Joel Parker <JSP0421@gmail.com>
2016-06-12 02:14:15 -0400

    jQuery moved to it's own file

---
+++ ez-css-jquery.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    if (!window.jQuery) {
+        // jQuery is not loaded
+        alert("jQuery required for EZ-CSS");
+    }
+};
+
+jQuery(function() {
+    jQuery("[class*='ez-']").each(function() {
+        var element = jQuery(this);
+        var classNames = element.attr("class").toString().split(' ');
+
+        $.each(classNames, function (i, className) {
+            var pos = className.indexOf('ez-');
+            if (pos > -1) {
+                var command = className.substr(pos + 3);
+                command = command.split('-');
+
+                for (i = 0; i < command.length; i++)
+                    command[i] = command[i].replace('_', '-');
+
+                console.log(command);
+
+                switch (command.length) {
+                    case 2:
+                        jQuery(element).css(command[0], command[1]);
+                        break;
+                    case 3:
+                        jQuery(element).css(command[0] + '-' + command[1], command[2]);
+                        break;
+                    case 4:
+                        jQuery(element).css(command[0] + '-' + command[1], command[3]);
+                        jQuery(element).css(command[0] + '-' + command[2], command[3]);
+                        break;
+                }
+            }
+        });
+    });
+});
