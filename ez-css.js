e9640f6a541c063547d2826de2eed3a3ae43da15
Joel Parker <JSP0421@gmail.com>
2016-06-12 02:14:06 -0400

    Vanilla JS completed

--- ez-css.js
+++ ez-css.js
@@ -1,37 +1,55 @@
 window.onload = function() {
-    if (!window.jQuery) {
-        // jQuery is not loaded
-        alert("jQuery required for EZ-CSS");
+    var matches = [];
+    var elems = document.getElementsByTagName("*");
+
+    for (var i=0; i<elems.length; i++) {
+        if (elems[i].className.indexOf("ez-") == 0)
+            matches.push(elems[i]);
+    }
+
+    var classes = [];
+    for(i = 0; i < matches.length; i++) {
+        classes.push(matches[i].className.split(' '));
+
+        for (j = 0; j < classes.length; j++) {
+            var pos;
+            for (k = 0; k < classes[j].length; k++) {
+                pos = classes[j][k].indexOf('ez-');
+
+                var command;
+                if (pos > -1) {
+                    command = classes[j][k].substr(pos + 3);
+                    command = command.split('-');
+
+                    // console.log(command.indexOf('_') > -1);
+
+                    if (command[0].indexOf('_') > -1) {
+                        command[0] = command[0].split('_');
+                        command[0] = command[0][0] + ucfirst(command[0][1]);
+                    }
+
+                    switch (command.length) {
+                        case 2:
+                            matches[i].style[command[0]] = command[1];
+                            break;
+                        case 3:
+                            command[0] = command[0] + ucfirst(command[1]);
+                            matches[i].style[command[0]] = command[2];
+                            break;
+                        case 4:
+                            command[1] = command[0] + ucfirst(command[1]);
+                            command[2] = command[0] + ucfirst(command[2]);
+                            matches[i].style[command[1]] = command[3];
+                            matches[i].style[command[2]] = command[3];
+                            break;
+                    }
+                }
+            }
+        }
     }
 };
 
-jQuery(function() {
-    jQuery("[class*='ez-']").each(function() {
-        var element = jQuery(this);
-        var classNames = element.attr("class").toString().split(' ');
+function ucfirst(word) {
+    return word.charAt(0).toUpperCase() + word.substr(1);
 
-        $.each(classNames, function (i, className) {
-            var pos = className.indexOf('ez-');
-            if (pos > -1) {
-                var command = className.substr(pos + 3);
-                command = command.split('-');
-
-                for (i = 0; i < command.length; i++)
-                    command[i] = command[i].replace('_', '-');
-
-                switch (command.length) {
-                    case 2:
-                        jQuery(element).css(command[0], command[1]);
-                        break;
-                    case 3:
-                        jQuery(element).css(command[0] + '-' + command[1], command[2]);
-                        break;
-                    case 4:
-                        jQuery(element).css(command[0] + '-' + command[1], command[3]);
-                        jQuery(element).css(command[0] + '-' + command[2], command[3]);
-                        break;
-                }
-            }
-        });
-    });
-});
+}
